55509e07877a33aa4e2a55cb11741c9c81a229d0
unknown <matnaranjo2020@gmail.com>
2022-01-28 20:22:21 -0500

    Added levelkeyfunction, for movement and desappearing of the key, TimeManager for every level, and basic pause function

--- Assets/Scripts/LevelKeyFunctions.cs
+++ Assets/Scripts/LevelKeyFunctions.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelKeyCollision : MonoBehaviour
+public class LevelKeyFunctions : MonoBehaviour
 {
+    [SerializeField]
+    private float Angle= 0f;
+
+    [SerializeField]
+    private Transform KeyHolder;
+    
     private string PlayerTag = "PlayerCharacter";
     // Start is called before the first frame update
     void Start()
     {
-        
+        Angle*= Time.fixedDeltaTime;
     }
 
     // Update is called once per frame
@@ -17,11 +23,19 @@ public class LevelKeyCollision : MonoBehaviour
         
     }
 
+    void FixedUpdate(){
+        LevelKeyMovement();
+    }
+
     private void OnCollisionEnter2D(Collision2D col){
 
         if (col.gameObject.CompareTag(PlayerTag)){
             Destroy(this.gameObject);
         }
 
+    }
+
+    private void LevelKeyMovement(){
+        transform.RotateAround(KeyHolder.gameObject.transform.position, KeyHolder.gameObject.transform.forward, -Angle);
     }
 }
